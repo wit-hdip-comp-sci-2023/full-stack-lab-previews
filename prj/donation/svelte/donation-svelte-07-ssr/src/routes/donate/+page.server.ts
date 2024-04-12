d9298a81075c0dc38f3da5f44bb4b653338fab60
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-12 10:02:50 +0100

    lab updates

--- prj/donation/svelte/donation-svelte-07-ssr/src/routes/donate/+page.server.ts
+++ prj/donation/svelte/donation-svelte-07-ssr/src/routes/donate/+page.server.ts
@@ -14,23 +14,21 @@ export const load: PageServerLoad = async ({ parent }) => {
 
 export const actions = {
   donate: async ({ request, cookies }) => {
-    const donationUser = cookies.get("donation-user") as string;
-    if (donationUser) {
-      console.log(donationUser);
-      const user = JSON.parse(donationUser) as Session;
-      const form = await request.formData();
-      console.log(form);
-      const donation = {
-        amount: form.get("amount") as unknown as number,
-        method: form.get("method") as string,
-        candidate: form.get("candidate") as string,
-        lat: form.get("lat") as unknown as number,
-        lng: form.get("lng") as unknown as number,
-        donor: user._id,
-        _id: ""
-      };
-      console.log(donation);
-      donationService.donate(donation, user);
+    const cookieStr = cookies.get("donation-user") as string;
+    if (cookieStr) {
+      const session = JSON.parse(cookieStr) as Session;
+      if (session) {
+        const form = await request.formData();
+        const donation = {
+          amount: form.get("amount") as unknown as number,
+          method: form.get("method") as string,
+          candidate: form.get("candidate") as string,
+          lat: form.get("lat") as unknown as number,
+          lng: form.get("lng") as unknown as number,
+          donor: session._id
+        };
+        donationService.donate(donation, session);
+      }
     }
   }
 };
