5cb1965ae9bdc7512c023c6261b315016ab18d38
Eamonn de Leastar <edeleastar@wit.ie>
2024-04-12 08:43:27 +0100

    ssr lab updates

--- prj/donation/svelte/donation-svelte-07-ssr/src/routes/charts/+page.server.ts
+++ prj/donation/svelte/donation-svelte-07-ssr/src/routes/charts/+page.server.ts
@@ -1,12 +1,10 @@
 import { donationService } from "$lib/services/donation-service";
 import { generateByCandidate, generateByMethod } from "$lib/services/donation-utils";
-import type { Session } from "$lib/types/donation-types";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ cookies }) => {
-  const cookieStr = cookies.get("donation-user") as string;
-  if (cookieStr) {
-    const session = JSON.parse(cookieStr) as Session;
+export const load: PageServerLoad = async ({ parent }) => {
+  const { session } = await parent();
+  if (session) {
     const donations = await donationService.getDonations(session);
     const candidates = await donationService.getCandidates(session);
     return {
